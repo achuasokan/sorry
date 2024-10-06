614ebd1892037e0e6af10aa47293145135c58031
achuasokan <achuasokan555@gmail.com>
2024-10-06 14:07:36 +0530

    second file

---
+++ server.js
@@ -0,0 +1,11 @@
+//importing the app
+import app from './app.js'
+
+//port
+const port=process.env.PORT || 8000
+
+
+//starting the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+})
